a8a8208c831f232c9f1a6362baefdd599117009b
Cosmografik <bretzel75@gmail.com>
2018-09-16 17:32:42 +0200

    POlishing le level art + adding UI animation (not finished) + music

---
+++ Plastiroid/Assets/_Scripts/Timer.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var timer : float = 10.0;
+
+function Update () {
+
+timer -= Time.deltaTime;
+
+if(timer <= 0){
+// FAIRE QQCH QAUND TIMER <= 0
+timer = 0;
+Debug.Log("Timer = 0");
+}
+}
+
+function OnGUI(){
+GUI.Box(new Rect(10,10,50,25), timer.ToString("0"));
+}
